f0a52c7902bd048439378eea730d8391691cb745
kyle <kyleflemington@gmail.com>
2017-04-27 09:33:02 -0700

    Jquery CSS, Append, Detach

--- ex1.js
+++ ex1.js
@@ -16,3 +16,42 @@ $(document).ready(function(){
 $(document).ready(function(){
   $(":nth-child(2)", $('article')).css("color","red");
 });
+
+$(document).ready(function(){
+  $('article').children(":nth-child(1+1)").css('color','red')
+});
+
+
+
+
+
+/* In this exercise, you’re going create two click events—one that adds another list item to the list below, and one that crosses off an item in the list when its “done” link is clicked: 
+
+You’ll need to think about traversing the DOM, and which of jQuery’s methods you’ll need to update the <ul> element’s content.
+
+You can complete this activity in a basic HTML file in a text editor of your choice (don’t forget to include the jQuery library in the footer of your file), or in Codepen (be sure to go into the Settings for your pen and check off the box to add the jQuery library) */
+
+
+/* <div class="list-editor">
+   <ul class="my-list">
+      <li>
+         <span>My to-do</span> <a href="#" class="delete">[done]</a>
+      </li>
+   </ul>
+   <input type="text" name="new-item" />
+   <button>Add an item</button>
+</div>
+*/
+
+$(document).ready(function(){
+	$('button').click(
+    function() {
+		  console.log('Add')
+      $('.my-list').append("<li>" + $('input:textbox').val()  + "</li>" + "<span>My to-do</span> <a href='#' class='delete'>" );
+    });
+  $('.delete').click(
+    function(){
+      console.log('Delete')
+      $(this).find().closet('li').detach()
+    });
+});
